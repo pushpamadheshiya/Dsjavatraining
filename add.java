def56e5000e97ec9a0953071b0d45ebaea2b8091
Pushpa Madheshiya <89408003+pushpamadheshiya@users.noreply.github.com>
2024-07-22 12:04:24 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,9 @@
+public class add {
+    public static void main(String[] args) {
+        int a=5;
+        float b= (float)7.5;
+        System.out.println(a+(int)b);
+        
+        System.out.println((float)a+b);
+    }
+}
