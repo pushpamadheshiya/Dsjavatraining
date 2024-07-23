3167e3aa3444e82fbae9fda9059a3d1134ae3502
Pushpa Madheshiya <89408003+pushpamadheshiya@users.noreply.github.com>
2024-07-23 12:08:02 +0530

    Add files via upload

---
+++ forwhiledowhile1.java
@@ -0,0 +1,27 @@
+public class forwhiledowhile1 {
+    public static void main(String[]args){
+
+    
+    int i=11;
+
+    for (i=1;i<11;i++){
+        System.out.print(i+" ");
+    }
+    System.out.println();
+
+    while(i>1){
+        i--;
+        System.out.print(i+" ");
+    }
+    System.out.println();
+
+    do{
+        System.out.print(i+" ");
+    
+        i++;
+     } while(i<11);
+    
+    
+}
+
+}
